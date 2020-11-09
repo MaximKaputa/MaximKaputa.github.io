9b426d9ad2a7588b433e7d6d7fd2f6774e335f94
Maks Kaputa <maksospiston44@gmail.com>
2020-11-09 13:00:56 +0200

    Test project commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: Scaffold(body: View()));
+  }
+}
+
+class View extends StatefulWidget {
+  ViewState createState() => ViewState();
+}
+
+class ViewState extends State {
+  Color colorCode = Colors.white;
+  final Random random = Random();
+  generateRandomColor() {
+    Color tmpColor = Color(Random().nextInt(0xffffffff)).withAlpha(0xff);
+
+    setState(() {
+      colorCode = tmpColor;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          generateRandomColor();
+        },
+        child: Scaffold(
+            backgroundColor: colorCode,
+            body: Center(
+                child: Container(
+              child: Text(
+                'Hey there',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.black87,
+                ),
+              ),
+            ))));
+  }
+}
